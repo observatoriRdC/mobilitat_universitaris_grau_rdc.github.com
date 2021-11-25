9ab99f9ee276b0cad8b489f09b270821a999df35
observatoriRdC <pousf@staperpetua.org>
2021-11-25 08:14:55 +0100

    Cicle i Grau

---
+++ data/EstudiantsdeCicleiGrauuniversitari_1.js
@@ -0,0 +1,19 @@
+var json_EstudiantsdeCicleiGrauuniversitari_1 = {
+"type": "FeatureCollection",
+"name": "EstudiantsdeCicleiGrauuniversitari_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "UNI": "UAB", "ESTUDIANTS": "798" }, "geometry": { "type": "Point", "coordinates": [ 2.106013317001866, 41.500355742861288 ] } },
+{ "type": "Feature", "properties": { "UNI": "UAO", "ESTUDIANTS": "23" }, "geometry": { "type": "Point", "coordinates": [ 2.126778026991307, 41.410253930002931 ] } },
+{ "type": "Feature", "properties": { "UNI": "UB", "ESTUDIANTS": "361" }, "geometry": { "type": "Point", "coordinates": [ 2.163726833463323, 41.387074320135397 ] } },
+{ "type": "Feature", "properties": { "UNI": "UdG", "ESTUDIANTS": "33" }, "geometry": { "type": "Point", "coordinates": [ 2.827190372662051, 41.985710754185256 ] } },
+{ "type": "Feature", "properties": { "UNI": "UdL", "ESTUDIANTS": "12" }, "geometry": { "type": "Point", "coordinates": [ 0.619638152736769, 41.614781425770055 ] } },
+{ "type": "Feature", "properties": { "UNI": "UIC", "ESTUDIANTS": "56" }, "geometry": { "type": "Point", "coordinates": [ 2.123532021706026, 41.405700906822737 ] } },
+{ "type": "Feature", "properties": { "UNI": "UOC", "ESTUDIANTS": "350" }, "geometry": { "type": "Point", "coordinates": [ 2.132846285255911, 41.414748053235101 ] } },
+{ "type": "Feature", "properties": { "UNI": "UPC", "ESTUDIANTS": "251" }, "geometry": { "type": "Point", "coordinates": [ 2.115413656865425, 41.387621194955699 ] } },
+{ "type": "Feature", "properties": { "UNI": "UPF", "ESTUDIANTS": "140" }, "geometry": { "type": "Point", "coordinates": [ 2.179799765374927, 41.378981550919747 ] } },
+{ "type": "Feature", "properties": { "UNI": "URL", "ESTUDIANTS": "69" }, "geometry": { "type": "Point", "coordinates": [ 2.166288300823854, 41.383528635389382 ] } },
+{ "type": "Feature", "properties": { "UNI": "URV", "ESTUDIANTS": "5" }, "geometry": { "type": "Point", "coordinates": [ 1.249953613699355, 41.122703091262942 ] } },
+{ "type": "Feature", "properties": { "UNI": "UVIC-UCC", "ESTUDIANTS": "84" }, "geometry": { "type": "Point", "coordinates": [ 2.247251447448373, 41.933735322249511 ] } }
+]
+}
